056401b6b4d84934170d8f9b23ea57832dbf2853
BranTran <tran2833@stthomas.edu>
2020-02-05 21:26:56 -0600

    Error check for integers

--- pointers.cpp
+++ pointers.cpp
@@ -17,21 +17,74 @@ int main(int argc, char **argv)
 {
     Student student;
     double average;
-
+    string input;
+    // Set up pointers
+    student.f_name = new char[128];
+    student.l_name = new char[128];
     // Sequence of user input -> store in fields of `student`
     cout << "Please enter the student's id number: ";
+    getline(cin, input);
+    while(cin.fail() || cin.eof() || input.find_first_not_of("0123456789") != string::npos){
+      cout << "Sorry, I cannot understand your answer" << endl;
+      if( input.find_first_not_of("0123456789") == string::npos){
+	cin.clear();
+	cin.ignore();
+      }
+
+      cout << "Please enter the student's id number: ";
+      
+      getline(cin,input);
+    }
+    string::size_type st;
+    student.id = stoi(input,&st);
+    
+    printf("We have an ID: %d\n",student.id);
+    
     cout << "Please enter the student's first name: ";
+    cin >> student.f_name;
     cout << "Please enter the student's last name: ";
+    cin >> student.l_name;
+
     cout << "Please enter how many assignments were graded: ";
+    getline(cin, input);
+    while(cin.fail() || cin.eof() || input.find_first_not_of("0123456789") != string::npos){
+      cout << "Sorry, I cannot understand your answer" << endl;
+      if( input.find_first_not_of("0123456789") == string::npos){
+	cin.clear();
+	cin.ignore();
+      }
+      cout << "Please enter how many assignments were graded: ";
+      
+      getline(cin,input);
+    }
+    student.n_assignments = stoi(input,&st);
+
+
+
+
+
     for(int i = 0; i < student.n_assignments; i++)
     {
-        cout << "Please enter grade for assignment "<<i<<": ";
+      cout << "Please enter grade for assignment "<<i<<": ";
+      getline(cin, input);
+      while(cin.fail() || cin.eof() || input.find_first_not_of("0123456789.") != string::npos){
+	cout << "Sorry, I cannot understand your answer" << endl;
+	if( input.find_first_not_of("0123456789.") == string::npos){
+	  cin.clear();
+	  cin.ignore();
+	}
+	cout << "Please enter grade for assignment "<<i<<": ";
+	
+	getline(cin,input);
+      }
+      student.grades[i] = stof(input,&st);	
     }
     
     // Call `CalculateStudentAverage(???, ???)`
-    CalculateStudentAverage(&student, &average);
+    calculateStudentAverage(&student, &average);
     // Output `average`
-    printf("Student: %s %s [%d]\d\tAverage grade: %.1f",student.f_name,student.l_name,student.id,average);
+    printf("Student: %s %s [%d]\n\tAverage grade: %.1f\n",student.f_name,student.l_name,student.id,average);
+    // */
     return 0;
 }
 
@@ -39,9 +92,9 @@ void calculateStudentAverage(void *object, double *avg)
 {
     Student* student = (Student*)object;//Get my student formatted right;
     // Code to calculate and store average grade
-    for(int i = 0; i < student.n_assignments; i++)
+    for(int i = 0; i < student->n_assignments; i++)
     {
-        *avg = *avg + student.grades[i];
+        *avg = *avg + student->grades[i];
     }
-    *avg = *avg / student.n_assignments;
+    *avg = *avg / student->n_assignments;
 }
