8d0d5ecc9831e8837c6300cc39b753546c3d8da3
BranTran <tran2833@stthomas.edu>
2020-02-10 10:25:23 -0600

    Finished with functionality, Issue with formatting

--- pointers.cpp
+++ pointers.cpp
@@ -1,6 +1,9 @@
 // compile: g++ -std=c++11 -o pointers pointers.cpp
 #include <iostream>
 #include <string>
+#include <cstring>
+#define NAMESIZE 128
+#define NUMSIZE 10
 using namespace std;
 
 typedef struct Student {
@@ -19,12 +22,12 @@ int main(int argc, char **argv)
     double average;
     string input;
     // Set up pointers
-    student.f_name = new char[128];
-    student.l_name = new char[128];
+    student.f_name = new char[NAMESIZE];
+    student.l_name = new char[NAMESIZE];
     // Sequence of user input -> store in fields of `student`
     cout << "Please enter the student's id number: ";
-    getline(cin, input);
-    while(cin.fail() || cin.eof() || input.find_first_not_of("0123456789") != string::npos){
+    cin >> input;
+    while(cin.fail() || input.find_first_not_of("0123456789") != string::npos){
       cout << "Sorry, I cannot understand your answer" << endl;
       if( input.find_first_not_of("0123456789") == string::npos){
 	cin.clear();
@@ -33,21 +36,31 @@ int main(int argc, char **argv)
 
       cout << "Please enter the student's id number: ";
       
-      getline(cin,input);
+      cin >> input;
     }
     string::size_type st;
     student.id = stoi(input,&st);
     
-    printf("We have an ID: %d\n",student.id);
+    //printf("We have an ID: %d\n",student.id);
     
+    //First name
     cout << "Please enter the student's first name: ";
-    cin >> student.f_name;
-    cout << "Please enter the student's last name: ";
-    cin >> student.l_name;
+    cin >> input;
+    strcpy(student.f_name,input.c_str());
 
+
+    //Last name
+    cout << "Please enter the student's last name: ";
+    cin >> input;
+    strcpy(student.l_name,input.c_str());
+    //printf("\n\nStudent: %s %s [%d]\n",student.f_name,student.l_name,student.id);
+
+    //Number of Assignments
     cout << "Please enter how many assignments were graded: ";
-    getline(cin, input);
-    while(cin.fail() || cin.eof() || input.find_first_not_of("0123456789") != string::npos){
+    input = "";
+    cin >> input;
+    //printf("getting input %s\n",input.c_str());
+    while(cin.fail() || input.find_first_not_of("0123456789") != string::npos){
       cout << "Sorry, I cannot understand your answer" << endl;
       if( input.find_first_not_of("0123456789") == string::npos){
 	cin.clear();
@@ -55,35 +68,39 @@ int main(int argc, char **argv)
       }
       cout << "Please enter how many assignments were graded: ";
       
-      getline(cin,input);
+      cin >> input;
     }
+    
     student.n_assignments = stoi(input,&st);
+    student.grades = new double[student.n_assignments];
 
 
-
-
-
+    input = "";
+    //Scores
     for(int i = 0; i < student.n_assignments; i++)
     {
       cout << "Please enter grade for assignment "<<i<<": ";
-      getline(cin, input);
-      while(cin.fail() || cin.eof() || input.find_first_not_of("0123456789.") != string::npos){
+      cin >> input;
+      while(cin.fail() || input.find_first_not_of("0123456789.") != string::npos){
 	cout << "Sorry, I cannot understand your answer" << endl;
 	if( input.find_first_not_of("0123456789.") == string::npos){
 	  cin.clear();
 	  cin.ignore();
+	  input = "";
 	}
 	cout << "Please enter grade for assignment "<<i<<": ";
-	
-	getline(cin,input);
+	cin >> input;
       }
-      student.grades[i] = stof(input,&st);	
+      //printf("Our input is %s\n",input.c_str());
+      student.grades[i] = std::stod(input);
+      //printf("Our input is %s, the value is %f\n",input.c_str(),student.grades[i]);
+      input = "";
     }
     
     // Call `CalculateStudentAverage(???, ???)`
     calculateStudentAverage(&student, &average);
     // Output `average`
-    printf("Student: %s %s [%d]\n\tAverage grade: %.1f\n",student.f_name,student.l_name,student.id,average);
+    printf("Student: %s %s [%d]\n  Average grade: %.1f\n",student.f_name,student.l_name,student.id,average);
     // */
     return 0;
 }
